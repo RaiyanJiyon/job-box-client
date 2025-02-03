f615588f413e09e89c30ab86b2872aeb6b26cf89
RaiyanJiyon <raiyanjiyon814@gmail.com>
2025-02-03 19:31:47 +0600

    Added job.json file for sample job data

--- src/pages/public/HomePage.jsx
+++ src/pages/public/HomePage.jsx
@@ -1,9 +1,16 @@
 import Hiring from "../../components/home/Hiring/Hiring";
+import Test from "../../components/home/Test/Test";
 
 const HomePage = () => {
     return (
+        <div>
         <div className="my-20">
             <Hiring />
+        </div>
+        <div className="my-20">
+            <Test />
+        </div>
+
         </div>
     );
 };
