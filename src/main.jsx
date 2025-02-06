1c0ffd0d47db2a58c8de57b4a5d7d7b12b7b4242
RaiyanJiyon <raiyanjiyon814@gmail.com>
2025-02-06 16:07:10 +0600

    Add custom hook useAuth for accessing AuthContext

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import {
 } from "react-router-dom";
 import "./index.css";
 import router from "./routes/Routes";
-import AuthProvider from "./context/AuthProvider";
+import { AuthProvider } from "./context/AuthProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
