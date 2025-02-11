cd717238ca32f4ae3136d31b3732f65608978548
RaiyanJiyon <raiyanjiyon814@gmail.com>
2025-02-11 11:05:36 +0600

    feat: Implement ProtectedRoute component with user authentication and loading state

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import useAuth from '../hooks/useAuth';
+import Loader from '../components/common/Loader/Loader';
+import { Navigate, useLocation } from 'react-router-dom';
 
-const ProtectedRoute = ({children}) => {
-    const {user, loading} = useAuth();
+const ProtectedRoute = ({ children }) => {
+    const { user, loading } = useAuth();
+    const location = useLocation();
 
     if (loading) {
-        
+        return <Loader />
+    };
+
+    if (user) {
+        return children
     }
-    return (
-        <div>
-            
-        </div>
-    );
+    return <Navigate to={'/login'} state={{ from: location }} replace />
 };
 
 export default ProtectedRoute;
