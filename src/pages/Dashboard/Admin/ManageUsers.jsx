8e38e321059f25498cf8d1994ea868c39e2b96ab
RaiyanJiyon <raiyanjiyon814@gmail.com>
2025-02-12 19:28:00 +0600

    Added Manage Users page table

--- src/pages/Dashboard/Admin/ManageUsers.jsx
+++ src/pages/Dashboard/Admin/ManageUsers.jsx
@@ -1,9 +1,83 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import useAxiosSecure from '../../../hooks/useAxiosSecure';
+import SectionTitle from '../../../components/common/SectionTitle';
 
 const ManageUsers = () => {
+    const [users, setUsers] = useState([]);
+    const axiosSecure = useAxiosSecure();
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            try {
+                const response = await axiosSecure.get('/users');
+                setUsers(response.data);
+            } catch (error) {
+                console.error(error.message);
+            }
+        };
+        fetchUsers();
+    }, []);
+
     return (
         <div>
-            
+            <SectionTitle title="All Users" description="Manage the people, change the role of the user and also you can delete the user" />
+            <div className="relative overflow-x-auto shadow-lg sm:rounded-lg mt-10 p-4 bg-white">
+                <div className='flex justify-between items-center mb-4'>
+                    <h2 className="text-lg font-semibold text-gray-700">User Management</h2>
+                    <div className="relative">
+                        <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                            <svg className="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                <path
+                                    stroke="currentColor"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                                />
+                            </svg>
+                        </div>
+                        <input
+                            type="text"
+                            id="table-search-users"
+                            className="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
+                            placeholder="Search for users"
+                        />
+                    </div>
+                </div>
+                <table className="w-full text-sm text-left text-gray-500">
+                    <thead className="text-xs text-gray-700 uppercase bg-gray-100">
+                        <tr>
+                            <th scope="col" className="px-6 py-3">Name</th>
+                            <th scope="col" className="px-6 py-3">Username</th>
+                            <th scope="col" className="px-6 py-3">Role</th>
+                            <th scope="col" className="px-6 py-3 text-center">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users.map(user => (
+                                <tr key={user._id} className="bg-white border-b border-gray-200 hover:bg-gray-50">
+                                    <th scope="row" className="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap">
+                                        <img className="w-10 h-10 rounded-full" src={`${user.photoURL}`} alt="User" />
+                                        <div className="ps-3">
+                                            <div className="text-base font-semibold">{user.name}</div>
+                                            <div className="font-normal text-gray-500">{user.email}</div>
+                                        </div>
+                                    </th>
+                                    <td className="px-6 py-4">{user.username}</td>
+                                    <td className="px-6 py-4 capitalize">{user.role}</td>
+                                    <td className="px-6 py-4 text-center">
+                                        <div className="flex justify-center space-x-4">
+                                            <button className="text-blue-600 hover:underline">Edit</button>
+                                            <button className="text-red-600 hover:underline">Delete</button>
+                                        </div>
+                                    </td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
